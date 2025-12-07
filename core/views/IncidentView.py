c80479541e71c9bf6c226c4835f673fae1ef1e92
anaellekabena <anaellekabena@gmail.com>
2025-12-07 08:37:20 +0100

    add pagination

--- core/views/IncidentView.py
+++ core/views/IncidentView.py
@@ -9,15 +9,21 @@ from core.serializers import IncidentSerializer
 
 class IncidentAPIView(APIView):
     permission_classes = [AllowAny]
-    parser_classes = [MultiPartParser, FormParser]  # pour gérer FormData + fichiers
+    parser_classes = [MultiPartParser, FormParser]
 
     def get(self, request):
-        incidents = Incident.objects.all()
+        incidents = Incident.objects.all().order_by('-id')
+
         paginator = PageNumberPagination()
         paginator.page_size = 10
+
+        # 👉 appliquer pagination
         paginated_incidents = paginator.paginate_queryset(incidents, request)
+
         serializer = IncidentSerializer(paginated_incidents, many=True)
-        return Response(serializer.data)
+
+        # 👉 retourner la réponse paginée (IMPORTANT)
+        return paginator.get_paginated_response(serializer.data)
 
     def post(self, request):
         serializer = IncidentSerializer(data=request.data)
@@ -26,7 +32,6 @@ class IncidentAPIView(APIView):
             incident = serializer.save()
             return Response(IncidentSerializer(incident).data, status=status.HTTP_201_CREATED)
         
-        # Si invalid, afficher les erreurs pour debug
         print("=== Serializer errors ===")
         for field, errors in serializer.errors.items():
             print(f"Champ problématique : {field} -> Erreurs : {errors}")
